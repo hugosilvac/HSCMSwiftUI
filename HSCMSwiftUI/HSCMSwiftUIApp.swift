3ebe0444d17479dfbfe73580943892f7b87494e5
Hugo Silva Cantanhede <hugosilvac@gmail.com>
2024-09-17 09:31:35 -0300

    Primeiro commit - criação do projeto

---
+++ HSCMSwiftUI/HSCMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSCMSwiftUIApp.swift
+//  HSCMSwiftUI
+//
+//  Created by Hugo Cantanhede on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HSCMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
